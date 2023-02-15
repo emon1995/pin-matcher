98664299f6565b73e6d8c3649c620592792dea49
emon1995 <emonhossain995@gmail.com>
2023-02-15 18:50:41 +0600

    design done

---
+++ js/app.js
@@ -0,0 +1,9 @@
+const m = Math.pow(4, 0.5) + "spiderman" + "987" + Math.sqrt(9);
+console.log(m);
+
+const product = 5;
+const price = "7";
+const subTotal = product * price;
+const tax = subTotal / 10;
+const total = subTotal + tax;
+console.log(total);
